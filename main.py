0c9ef2e355191c9d42c6a9c07005ced8d6691713
Andreas Calleja <andreascalleja@gmail.com>
2022-11-02 21:04:26 +0100

    Code refactoring

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import pandas as pd
-from tabulate import tabulate
 import matplotlib.pyplot as plt
-
 from PIL import Image
 import urllib.request
-import os
 
 def ax_logo(team_id, ax):
     '''
@@ -43,148 +40,146 @@ def minutes_battery(minutes, ax):
     ax.set_axis_off()
     return ax
 
-url = 'https://fbref.com/en/squads/19538871/2022-2023/all_comps/Manchester-United-Stats-All-Competitions'
-html = pd.read_html(url, header=1)
-# print(html)
-df = html[0]
-# print(df)
-# print('------------------------------------------------------')
-# df = html[1]
-# print(df)
-# print('------------------------------------------------------')
-# df = html[2]
-# print(df)
-# print('------------------------------------------------------')
-df = html[28]
-df = pd.read_csv('man_utd_playing_times.csv', header=[0])
 
-df_final = df[df['Min'] >= 400].reset_index(drop=True)
-df_final = df_final[['Player', 'Nation', 'Pos', 'Min', 'MP', 'Starts', 'Subs', 'unSub']]
-df_final['InSquad'] = df_final['MP'] + df_final['unSub']
-df_final = df_final.sort_values(by='Min').reset_index(drop=True)
-df_final = df_final[~df_final['Pos'].isna()]
-df_final['Nation'] = [x.split(' ')[1].lower() for x in df_final['Nation']]
-df_final['Starts_InSquad'] = [f'{x}/{y}' for x,y in zip(df_final['Starts'], df_final['InSquad'])]
-print(df)
+def save_figure(fig_name, dpi, transparency, face_color, bbox):
+    plt.savefig(fig_name, 
+    dpi=dpi,
+    transparent=transparency,
+    facecolor=face_color,
+    bbox_inches=bbox
+)
 
-fig = plt.figure(figsize=(8,10), dpi=300, facecolor='#EFE9E6')
-ax = plt.subplot()
 
-ncols = 6
-nrows = df_final.shape[0]
+def get_data():
+    url = 'https://fbref.com/en/squads/19538871/2022-2023/all_comps/Manchester-United-Stats-All-Competitions'
+    html = pd.read_html(url, header=1)
+    # df = html[28]
+    df = pd.read_csv('real_madrid_playing_time.csv', header=[0])
+    return df
 
-ax.set_xlim(0, ncols + 1)
-ax.set_ylim(0, nrows + 1)
 
-positions = [0.25, 3.5, 4.5, 5.5, 6.5]
-columns = ['Player', 'Pos', 'Min', 'MP', 'Starts_InSquad']
+def format_data(df):
+    df_final = df[df['Min'] >= 400].reset_index(drop=True)
+    df_final = df_final[['Player', 'Nation', 'Pos', 'Min', 'MP', 'Starts', 'Subs', 'unSub']]
+    df_final['InSquad'] = df_final['MP'] + df_final['unSub']
+    df_final = df_final.sort_values(by='Min').reset_index(drop=True)
+    df_final = df_final[~df_final['Pos'].isna()]
+    df_final['Nation'] = [x.split(' ')[1].lower() for x in df_final['Nation']]
+    df_final['Starts_InSquad'] = [f'{x}/{y}' for x,y in zip(df_final['Starts'], df_final['InSquad'])]
+    return df_final
 
-for i in range(nrows):
-    for j, column in enumerate(columns):
-        if j == 0:
-            ha = 'left'
-        else:
-            ha = 'center'
-        if column == 'Min':
-            continue
-        else:
-            text_label = f'{df_final[column].iloc[i]}'
-            weight = 'normal'
-        ax.annotate(
-            xy=(positions[j], i + .5),
-            text=text_label,
-            ha=ha,
-            va='center',
-            weight=weight
+
+def plot_data(df_final):
+    fig = plt.figure(figsize=(8,10), dpi=300, facecolor='#EFE9E6')
+    ax = plt.subplot()
+
+    ncols = 6
+    nrows = df_final.shape[0]
+
+    ax.set_xlim(0, ncols + 1)
+    ax.set_ylim(0, nrows + 1)
+
+    positions = [0.25, 3.5, 4.5, 5.5, 6.5]
+    columns = ['Player', 'Pos', 'Min', 'MP', 'Starts_InSquad']
+
+    for i in range(nrows):
+        for j, column in enumerate(columns):
+            if j == 0:
+                ha = 'left'
+            else:
+                ha = 'center'
+            if column == 'Min':
+                continue
+            else:
+                text_label = f'{df_final[column].iloc[i]}'
+                weight = 'normal'
+            ax.annotate(
+                xy=(positions[j], i + .5),
+                text=text_label,
+                ha=ha,
+                va='center',
+                weight=weight
+            )
+
+    DC_to_FC = ax.transData.transform
+    FC_to_NFC = fig.transFigure.inverted().transform
+
+    DC_to_NFC = lambda x: FC_to_NFC(DC_to_FC(x))
+
+    ax_point_1 = DC_to_NFC([2.25, 0.25])
+    ax_point_2 = DC_to_NFC([2.75, 0.75])
+    ax_width = abs(ax_point_1[0] - ax_point_2[0])
+    ax_height = abs(ax_point_1[1] - ax_point_2[1])
+
+    for x in range(0, nrows):
+        ax_coords = DC_to_NFC([2.25, x + .25])
+        flag_ax = fig.add_axes(
+            [ax_coords[0], ax_coords[1], ax_width, ax_height]
         )
+        ax_logo(df_final['Nation'].iloc[x], flag_ax)
 
-DC_to_FC = ax.transData.transform
-FC_to_NFC = fig.transFigure.inverted().transform
-
-DC_to_NFC = lambda x: FC_to_NFC(DC_to_FC(x))
-
-ax_point_1 = DC_to_NFC([2.25, 0.25])
-ax_point_2 = DC_to_NFC([2.75, 0.75])
-ax_width = abs(ax_point_1[0] - ax_point_2[0])
-ax_height = abs(ax_point_1[1] - ax_point_2[1])
-
-for x in range(0, nrows):
-    ax_coords = DC_to_NFC([2.25, x + .25])
-    flag_ax = fig.add_axes(
-        [ax_coords[0], ax_coords[1], ax_width, ax_height]
-    )
-    ax_logo(df_final['Nation'].iloc[x], flag_ax)
-
-ax_point_1 = DC_to_NFC([4, 0.05])
-ax_point_2 = DC_to_NFC([5, 0.95])
-ax_width = abs(ax_point_1[0] - ax_point_2[0])
-ax_height = abs(ax_point_1[1] - ax_point_2[1])
-for x in range(0, nrows):
-    ax_coords = DC_to_NFC([4, x + .025])
-    bar_ax = fig.add_axes(
-        [ax_coords[0], ax_coords[1], ax_width, ax_height]
-    )
-    minutes_battery(df_final['Min'].iloc[x], bar_ax)
-
-column_names = ['Player', 'Position', '% of Min.\nPlayed', 'Matches\nPlayed', 'Starts /\nIn Squad']
-for index, c in enumerate(column_names):
-        if index == 0:
-            ha = 'left'
-        else:
-            ha = 'center'
-        ax.annotate(
-            xy=(positions[index], nrows + .25),
-            text=column_names[index],
-            ha=ha,
-            va='bottom',
-            weight='bold'
+    ax_point_1 = DC_to_NFC([4, 0.05])
+    ax_point_2 = DC_to_NFC([5, 0.95])
+    ax_width = abs(ax_point_1[0] - ax_point_2[0])
+    ax_height = abs(ax_point_1[1] - ax_point_2[1])
+    for x in range(0, nrows):
+        ax_coords = DC_to_NFC([4, x + .025])
+        bar_ax = fig.add_axes(
+            [ax_coords[0], ax_coords[1], ax_width, ax_height]
         )
+        minutes_battery(df_final['Min'].iloc[x], bar_ax)
 
-ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [nrows, nrows], lw=1.5, color='black', marker='', zorder=4)
-ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [0, 0], lw=1.5, color='black', marker='', zorder=4)
-for x in range(1, nrows):
-    ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [x, x], lw=1.15, color='gray', ls=':', zorder=3 , marker='')
+    column_names = ['Player', 'Position', '% of Min.\nPlayed', 'Matches\nPlayed', 'Starts /\nIn Squad']
+    for index, c in enumerate(column_names):
+            if index == 0:
+                ha = 'left'
+            else:
+                ha = 'center'
+            ax.annotate(
+                xy=(positions[index], nrows + .25),
+                text=column_names[index],
+                ha=ha,
+                va='bottom',
+                weight='bold'
+            )
 
-ax.fill_between(
-    x=[0,2],
-    y1=nrows,
-    y2=0,
-    color='lightgrey',
-    alpha=0.5,
-    ec='None'
-)
+    ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [nrows, nrows], lw=1.5, color='black', marker='', zorder=4)
+    ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [0, 0], lw=1.5, color='black', marker='', zorder=4)
+    for x in range(1, nrows):
+        ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [x, x], lw=1.15, color='gray', ls=':', zorder=3 , marker='')
 
-ax.set_axis_off()
-logo_ax = fig.add_axes(
-    [0.825, 0.89, .05, .05]
-)
-ax_logo(8633, logo_ax)
-fig.text(
-    x=0.15, y=.91,
-    s='Real Madrid 2021/2022 Squad Playing Time',
-    ha='left',
-    va='bottom',
-    weight='bold',
-    size=12
-)
+    ax.fill_between(
+        x=[0,2],
+        y1=nrows,
+        y2=0,
+        color='lightgrey',
+        alpha=0.5,
+        ec='None'
+    )
 
-plt.savefig(
-    'figures/final_table.png',
-    dpi=300,
-    transparent=True,
-    bbox_inches='tight'
-)
+    ax.set_axis_off()
+    logo_ax = fig.add_axes(
+        [0.825, 0.89, .05, .05]
+    )
+    ax_logo(8633, logo_ax)
+    fig.text(
+        x=0.15, y=.91,
+        s='Real Madrid 2021/2022 Squad Playing Time',
+        ha='left',
+        va='bottom',
+        weight='bold',
+        size=12
+    )
 
-plt.savefig(
-    'figures/final_table_bkg.png',
-    dpi=300,
-    transparent=False,
-    facecolor='#EFE9E6',
-    bbox_inches='tight'
-)
 
-#TODO: Make code in functions
-#TODO: Get data from url - not always table 25
-#TODO: Get team as input
-#TODO: Make for not a full season
-#TODO: Redo code
+    save_figure('figures/final_table.png', 300, True, '#EFE9E6', 'tight')
+    save_figure('figures/final_table_bkg.png', 300, False, '#EFE9E6', 'tight')
+
+
+def main():
+    df = get_data()
+    df = format_data(df)
+    df = plot_data(df)
+
+
+main()
