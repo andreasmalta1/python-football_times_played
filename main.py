e15da4dd5666a0073ee1f55dafdaf6b97c1b2e34
Andreas Calleja <andreascalleja@gmail.com>
2022-11-15 23:19:57 +0100

    Working for mid season break

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from PIL import Image
 import urllib.request
 
 def ax_logo(team_id, ax):
-    '''
-    Plots the logo of the team at a specific axes.
-    Args:
-        team_id (int): the id of the team according to Fotmob. You can find it in the url of the team page.
-        ax (object): the matplotlib axes where we'll draw the image.
-    '''
     fotmob_url = 'https://images.fotmob.com/image_resources/logo/teamlogo/'
     club_icon = Image.open(urllib.request.urlopen(f'{fotmob_url}{team_id}.png'))
     ax.imshow(club_icon)
@@ -18,25 +12,20 @@ def ax_logo(team_id, ax):
 
 
 def minutes_battery(minutes, ax):
-    '''
-    This function takes an integer and an axes and 
-    plots a battery chart.
-    '''
     ax.set_xlim(0,1)
     ax.set_ylim(0,1)
     ax.barh([0.5], [1], fc = '#EFE9E6', ec='black', height=.35)
-    ax.barh([0.5], [minutes/(90*38)], fc = '#00529F', height=.35)
+    ax.barh([0.5], [minutes/(90*21)], fc = '#00529F', height=.35)
     text_ = ax.annotate(
-        xy=(minutes/(90*38), .5),
-        text=f'{minutes/(90*38):.0%}',
+        xy=(minutes/(90*21), .5),
+        text=f'{minutes/(90*21):.0%}',
         xytext=(-8,0),
         textcoords='offset points',
         weight='bold',
         color='#EFE9E6',
         va='center',
         ha='center',
-        size=5
-    )
+        size=5)
     ax.set_axis_off()
     return ax
 
@@ -46,15 +35,11 @@ def save_figure(fig_name, dpi, transparency, face_color, bbox):
     dpi=dpi,
     transparent=transparency,
     facecolor=face_color,
-    bbox_inches=bbox
-)
+    bbox_inches=bbox)
 
 
 def get_data():
-    url = 'https://fbref.com/en/squads/19538871/2022-2023/all_comps/Manchester-United-Stats-All-Competitions'
-    html = pd.read_html(url, header=1)
-    # df = html[28]
-    df = pd.read_csv('real_madrid_playing_time.csv', header=[0])
+    df = pd.read_csv('csvs/man_utd.csv', header=[0])
     return df
 
 
@@ -88,18 +73,22 @@ def plot_data(df_final):
                 ha = 'left'
             else:
                 ha = 'center'
+            
             if column == 'Min':
                 continue
-            else:
-                text_label = f'{df_final[column].iloc[i]}'
-                weight = 'normal'
+            
+            text_label = f'{df_final[column].iloc[i]}'
+            weight = 'normal'
+
+            if column == 'Pos':
+                text_label = text_label[:2]
+            
             ax.annotate(
                 xy=(positions[j], i + .5),
                 text=text_label,
                 ha=ha,
                 va='center',
-                weight=weight
-            )
+                weight=weight)
 
     DC_to_FC = ax.transData.transform
     FC_to_NFC = fig.transFigure.inverted().transform
@@ -113,38 +102,37 @@ def plot_data(df_final):
 
     for x in range(0, nrows):
         ax_coords = DC_to_NFC([2.25, x + .25])
-        flag_ax = fig.add_axes(
-            [ax_coords[0], ax_coords[1], ax_width, ax_height]
-        )
+        flag_ax = fig.add_axes([ax_coords[0], ax_coords[1], ax_width, ax_height])
         ax_logo(df_final['Nation'].iloc[x], flag_ax)
 
     ax_point_1 = DC_to_NFC([4, 0.05])
     ax_point_2 = DC_to_NFC([5, 0.95])
     ax_width = abs(ax_point_1[0] - ax_point_2[0])
     ax_height = abs(ax_point_1[1] - ax_point_2[1])
+    
     for x in range(0, nrows):
         ax_coords = DC_to_NFC([4, x + .025])
-        bar_ax = fig.add_axes(
-            [ax_coords[0], ax_coords[1], ax_width, ax_height]
-        )
+        bar_ax = fig.add_axes([ax_coords[0], ax_coords[1], ax_width, ax_height])
         minutes_battery(df_final['Min'].iloc[x], bar_ax)
 
     column_names = ['Player', 'Position', '% of Min.\nPlayed', 'Matches\nPlayed', 'Starts /\nIn Squad']
+    
     for index, c in enumerate(column_names):
             if index == 0:
                 ha = 'left'
             else:
                 ha = 'center'
+            
             ax.annotate(
                 xy=(positions[index], nrows + .25),
                 text=column_names[index],
                 ha=ha,
                 va='bottom',
-                weight='bold'
-            )
+                weight='bold')
 
     ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [nrows, nrows], lw=1.5, color='black', marker='', zorder=4)
     ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [0, 0], lw=1.5, color='black', marker='', zorder=4)
+    
     for x in range(1, nrows):
         ax.plot([ax.get_xlim()[0], ax.get_xlim()[1]], [x, x], lw=1.15, color='gray', ls=':', zorder=3 , marker='')
 
@@ -154,23 +142,20 @@ def plot_data(df_final):
         y2=0,
         color='lightgrey',
         alpha=0.5,
-        ec='None'
-    )
+        ec='None')
 
     ax.set_axis_off()
-    logo_ax = fig.add_axes(
-        [0.825, 0.89, .05, .05]
-    )
-    ax_logo(8633, logo_ax)
+    
+    logo_ax = fig.add_axes([0.825, 0.89, .05, .05])
+    ax_logo(10260, logo_ax)
+    
     fig.text(
         x=0.15, y=.91,
-        s='Real Madrid 2021/2022 Squad Playing Time',
+        s='Man UTD 2022/2022 Squad Playing Time All Comps',
         ha='left',
         va='bottom',
         weight='bold',
-        size=12
-    )
-
+        size=12)
 
     save_figure('figures/final_table.png', 300, True, '#EFE9E6', 'tight')
     save_figure('figures/final_table_bkg.png', 300, False, '#EFE9E6', 'tight')
@@ -179,7 +164,9 @@ def plot_data(df_final):
 def main():
     df = get_data()
     df = format_data(df)
-    df = plot_data(df)
+    plot_data(df)
 
 
 main()
+
+# Footnotes
